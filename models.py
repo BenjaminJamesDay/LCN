f1d52c9ef8f7b6c4b0f382c5401e193c7f73bb55
BenjaminJamesDay <bjd39@cam.ac.uk>
2018-09-27 12:12:49 +0100

    Added verbose mode for debugging memory issues

--- models.py
+++ models.py
@@ -23,3 +23,26 @@ class latentConditionerModel10(nn.Module):
         xHat = self.decoder(l, decoderConditionals)
         
         return [yHat, xHat]
+
+class lcm10overbose(nn.Module):
+    def __init__(self):
+        super(latentConditionerModel10, self).__init__()
+        
+        self.encoder = parts.encoder()
+        self.latent2classifier = parts.latent2classifier()
+        self.classifier = parts.classifier(10)
+        self.class2decoder = parts.class2decoder(10)
+        self.decoder = parts.decoder()
+        
+    def forward(self, x):
+        l = self.encoder(x)
+        print('Encoded...\n')
+        classConditionals = self.latent2classifier(l)
+        print('Made conditionals from latent embedding...\n')
+        yHat = self.classifier(x, classConditionals)
+        print('Classified...\n')
+        decoderConditionals = self.class2decoder(yHat)
+        print('Made conditionals from classification...\n')
+        xHat = self.decoder(l, decoderConditionals)
+        print('Replicated from embedding...\n','Pass Complete.\n')
+        return [yHat, xHat]
