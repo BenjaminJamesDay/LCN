a169b3bf0d8243ea6bd9e8f07f33eb0f891d22af
BenjaminJamesDay <bjd39@cam.ac.uk>
2018-10-02 10:28:34 +0100

    Had max_epochs being defined twice

--- training.py
+++ training.py
@@ -27,7 +27,6 @@ trainloader, validloader = trainvalid
 testloader = data_loader_CIFAR10.get_test_loader('./data', batch_size=batchsize)
 
 # Run parameters
-max_epochs = 500
 time_label = time.strftime('%Y%m%d%H%M')
 valacc_save = './logs/' + time_label + '_VA.csv'
 tests_save = './logs/' + time_label + '_TA.csv'
